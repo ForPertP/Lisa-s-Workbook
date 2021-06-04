abf4a61a6a96341b508eeaaabb77ad51158cdf60
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2021-06-04 23:15:59 +0900

    Update Lisa-s-Workbook.cpp

--- Lisa-s-Workbook.cpp
+++ Lisa-s-Workbook.cpp
@@ -1,1 +1,19 @@
-
+int workbook(int n, int k, vector<int> arr)
+{
+    int count = 0;
+    int page = 0;
+    
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 1; j <= arr[i]; ++j)
+        { 
+            if (k == 1 || j % k == 1)
+                page++;
+            
+            if (j == page)
+                count++;
+        }
+    }
+    
+    return count;
+}
