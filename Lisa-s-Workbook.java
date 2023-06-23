5c58f4a6a0b1528c612072b2a3a279bfcb34fded
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2023-06-23 16:38:41 +0900

    Update Lisa-s-Workbook.java

--- Lisa-s-Workbook.java
+++ Lisa-s-Workbook.java
@@ -23,6 +23,21 @@ class Result {
      */
 
     public static int workbook(int n, int k, List<Integer> arr) {
+    // Write your code here
+        int count = 0;
+        int page = 0;
+
+        for (int i = 0; i < n; ++i) {
+            for (int j = 1; j <= arr.get(i); ++j) {
+                if (k == 1 || j % k == 1)
+                    page++;
+
+                if (j == page)
+                    count++;
+            }
+        }
+
+        return count;
     }
 
 }
@@ -51,4 +66,3 @@ public class Solution {
         bufferedWriter.close();
     }
 }
-
